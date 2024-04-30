fd9bc210835a8afc43c72c5635c9668c9696940c
mmwandera <mmwandera74@gmail.com>
2024-04-30 14:49:42 +0300

    added banner image

--- src/main-app/Content.jsx
+++ src/main-app/Content.jsx
@@ -1,7 +1,23 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import banner from "../assets/banner.png";
+
 export default function Content() {
   return (
-    <div>
-      <h1>Content</h1>
+    <div className="relative text-center">
+      <div className="absolute top-16 left-1/2 transform -translate-x-1/2">
+        <h1 className="text-white text-4xl font-bold mb-4">Get ahead, stay ahead</h1>
+        <p className="text-white">we help you stay ahead by offering courses that teach the latest skills. With expert instructors and interactive content, </p>
+        <p className="text-white">you&apos;ll be equipped to thrive in today&apos;s ever-changing world. Join us and start learning today</p>
+      </div>
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white bg-opacity-75 p-4 rounded-md mt-10">
+        <div className="flex w-full max-w-md items-center space-x-2">
+          <Input type="text" placeholder="Search for Courses" className="w-full" />
+          <Button type="submit">Search</Button>
+        </div>
+        {/* Add any other elements related to the search bar */}
+      </div>
+      <img src={banner} alt="" className="w-full h-auto max-h-500" style={{ maxHeight: '500px' }} />
     </div>
-  )
+  );
 }
--- src/main-app/Header.jsx
+++ src/main-app/Header.jsx
@@ -1,18 +1,18 @@
 import { Button } from "@/components/ui/button"
-import { Contact, Info, LayoutDashboard, Search } from "lucide-react"
+import { Contact, Info, LayoutDashboard, LibraryBig, Search } from "lucide-react"
 import logo from "../assets/logo.svg"
 
 export default function Header() {
   return (
     
-    <div className="flex justify-between p-10">
+    <div className="flex justify-between p-20 border-2 border-gray-200">
       <div>
         {/* logo */}
         <img src={logo} alt="logo" />
       </div>
       <div>
         <Button variant="ghost">
-          <LayoutDashboard className="mr-2 h-4 w-4" /> My Courses
+          <LibraryBig className="mr-2 h-4 w-4" /> My Courses
         </Button>
         <Button variant="ghost">
           <Search className="mr-2 h-4 w-4" /> Explore
@@ -22,6 +22,9 @@ export default function Header() {
         </Button>
         <Button variant="ghost">
           <Contact className="mr-2 h-4 w-4" /> Contact Us
+        </Button>
+        <Button variant="ghost">
+          <LayoutDashboard className="mr-2 h-4 w-4" /> Teacher&apos;s Portal
         </Button>
       </div>
       <div className="border-l-2 pl-2">
